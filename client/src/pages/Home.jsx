059c589005f34ff3bfb0e1832dafd3b038d02845
MarieAmp <marymary09@hotmail.com>
2020-07-27 16:46:24 +0200

    map first step

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import ReactMapboxGl from "react-mapbox-gl";
+import { Layer, Feature } from "react-mapbox-gl";
 
 const Home = (props) => {
-  // Implement react map box here.
+  const Map = ReactMapboxGl({
+    accessToken:
+      "pk.eyJ1IjoibWFyaWVhbXAiLCJhIjoiY2tkNGh5NnliMXJhcTJzbzdpbnZpNmw1bCJ9.rQjywXc3E0QEzGao20SB0A",
+  });
+
   return (
     <div>
-      <h1>MAPBOX MAP HERE</h1>
-      <p>On home /</p>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </Map>
+      ;
     </div>
   );
 };
